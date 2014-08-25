47386e44e3ad61f92e6ea2beea56ec170425452f
Xin-Xin Wang <xin-xinw@musiclifephilosophy.com>
2014-08-25 18:06:26 -0400

    automatically does git dir if no arguments

--- stat.bash
+++ stat.bash
@@ -1,15 +1,31 @@
 #!/bin/bash
 
 function git-stat {
-  for dir in */; do
-    cd "$dir"
+  local orig="$PWD"   # original directory
+  
+  local base="$1"
+  [ -z "$base" ] && base="/home/secret2008/Documents/Program/git"
+  
+  cd "$base"
+  if [ "$(echo */)" != "*/" ]; then
+    for dir in */; do
+      cd "$dir"
+      local str="$(git status --porcelain 2>&1)"
+      if [ -n "$str" ]; then
+        echo "$dir"
+        git status --porcelain
+      fi
+      cd ../
+    done
+  else
     local str="$(git status --porcelain 2>&1)"
     if [ -n "$str" ]; then
-      echo "$dir"
+      echo "./"
       git status --porcelain
     fi
-    cd ../
-  done
+  fi
+  
+  cd "$orig"
 }
 
-git-stat
+git-stat "$@"
