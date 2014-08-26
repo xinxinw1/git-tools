2a179a21c42bc58f39ee7ef9c8481d9af4454122
Xin-Xin Wang <xin-xinw@musiclifephilosophy.com>
2014-08-25 20:54:39 -0400

    run makes new dir in git-preview

--- run.bash
+++ run.bash
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 function run {
+  local d="${PWD##*/}"   # original directory
+  
   local bran="$1"
   [ -z "$1" ] && bran="gh-pages"
   
   # http://stackoverflow.com/questions/5167957/is-there-a-better-way-to-find-out-if-a-local-git-branch-exists
   if git show-ref --verify --quiet "refs/heads/$bran"; then
-    rm -r /srv/http/git-preview/* 2>/dev/null
+    local dir="/srv/http/git-preview/$d"
+    
+    rm -r "$dir"/* 2>/dev/null
+    mkdir "$dir"
     
     local b="$(git rev-parse --abbrev-ref HEAD)"
     if [ "$b" == "$bran" ]; then
-      cp -r * /srv/http/git-preview
+      cp -r * "$dir"
     else
-      git archive "$bran" | tar -xC /srv/http/git-preview
+      git archive "$bran" | tar -xC "$dir"
     fi
     
-    git deps -l -f /srv/http/git-preview/deps -o /srv/http/git-preview
+    git deps -l -f "$dir/deps" -o "$dir"
   else
     echo "Error: no branch $bran" >&2
   fi
