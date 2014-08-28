12c3e7aa8c9935556ac15c427ff5d8e1fa069b29
Xin-Xin Wang <xin-xinw@musiclifephilosophy.com>
2014-08-28 15:58:39 -0400

    add rstat

---
+++ rstat.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function git-rstat {
+  local orig="$PWD"   # original directory
+  
+  local base="$1"
+  [ -z "$base" ] && base="/home/secret2008/Documents/Program/git"
+  
+  cd "$base"
+  if [ "$(echo */)" != "*/" ]; then
+    for dir in */; do
+      cd "$dir"
+      local str="$(git status -b --porcelain | head -n 1 | cut -d' ' -f 3- 2>&1)"
+      if [ -n "$str" ]; then
+        echo "$dir"
+        git status -b --porcelain | head -n 1
+      fi
+      cd ../
+    done
+  else
+    local str="$(git status -b --porcelain | head -n 1 | cut -d' ' -f 3- 2>&1)"
+    if [ -n "$str" ]; then
+      echo "./"
+      git status -b --porcelain | head -n 1
+    fi
+  fi
+  
+  cd "$orig"
+}
+
+git-rstat "$@"
