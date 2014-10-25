5aa91609a776309969b944696a02d477e0d88f34
Xin-Xin Wang <xin-xinw@musiclifephilosophy.com>
2014-10-25 14:23:57 -0400

    upd is now quieter

--- upd.bash
+++ upd.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function redeps {
+function upd {
   local b="$(git rev-parse --abbrev-ref HEAD)"
   # if in detached head, get current revision
   # http://stackoverflow.com/questions/5724513/in-git-how-do-i-figure-out-what-my-current-revision-is
@@ -14,14 +14,14 @@ function redeps {
       git deps -c
       #git push origin "$bran"
     else
-      git checkout "$bran" || exit 1
-      git deps -c
+      git checkout -q "$bran" || exit 1
+      git deps -c >/dev/null
       #git push origin "$bran"
-      git checkout "$b"
+      git checkout -q "$b"
     fi
   else
     echo "Error: no branch $bran" >&2
   fi
 }
 
-redeps "$@"
+upd "$@"
